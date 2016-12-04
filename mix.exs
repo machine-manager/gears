fda35e538dbc0ae64ec74134e297d9de3d428f22
Ivan Kozik <ivan@ludios.org>
2016-12-04 18:26:33 +0000

    mix.exs: spaces -> tabs

--- mix.exs
+++ mix.exs
@@ -1,16 +1,18 @@
 defmodule Gears.Mixfile do
-  use Mix.Project
+	use Mix.Project
 
-  def project do
-    [app: :gears,
-     version: "0.3.0",
-     elixir: "~> 1.4-dev",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
-  end
+	def project do
+		[
+			app: :gears,
+			version: "0.3.0",
+			elixir: "~> 1.4-dev",
+			build_embedded: Mix.env == :prod,
+			start_permanent: Mix.env == :prod,
+			deps: deps()
+		]
+	end
 
-  defp deps do
-    []
-  end
+	defp deps do
+		[]
+	end
 end
