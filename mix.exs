0010d2bfb7b7dae8df09276827a495af6d9a48a1
Ivan Kozik <ivan@ludios.org>
2016-12-26 06:32:31 +0000

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gears.Mixfile do
 	def project do
 		[
 			app: :gears,
-			version: "0.6.2",
+			version: "0.7.0",
 			elixir: "~> 1.4",
 			build_embedded: Mix.env == :prod,
 			start_permanent: Mix.env == :prod,
